866888bb35fcaaf331f1bb47a1f65a6ea4074c81
Julien Lemaître <julien@lemaitre-creation.fr>
2017-10-16 18:21:25 +0200

    lesson 2 correction

--- src/api/resources/user/user.restRouter.js
+++ src/api/resources/user/user.restRouter.js
@@ -3,6 +3,7 @@ import userController from './user.controller'
 
 export const userRouter = express.Router()
 
+// ILike a middleware, it's doing a job on the param, and pass it next to the route which follow
 userRouter.param('id', userController.findByParam)
 
 userRouter.route('/')
