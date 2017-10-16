5e6229ab604ce0d6deeed305fd527657c08767e5
Julien Lemaître <julien@lemaitre-creation.fr>
2017-10-16 17:06:48 +0200

    lesson 1 attempt

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import http from 'http'
 import { createServer } from 'http'
 
 import app from './server'
-let currentApp = app
-
+let currentApp = app;
 
 if (module.hot) {
 	module.hot.accept(['./server'], () => {
-		server.removeListener('request', currentApp)
-		server.on('request', app)
+		server.removeListener('request', currentApp);
+		server.on('request', app);
 		currentApp = app
 	})
 }
+
+currentApp.get('/', (req, res) => {
+	res.json({received: true})
+});
+
+app.listen(3000, () => {
+	console.log('first server response sent !')
+});
